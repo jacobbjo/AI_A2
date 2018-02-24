711d0de88b30002d42b80628b565620f49121583
jacobbjo <jacob.bjorkman@live.com>
2018-02-24 11:47:51 +0100

    Structuring..

--- P1/agent.py
+++ P1/agent.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from math import atan2
 
 class Agent(object):
 
@@ -13,17 +13,55 @@ class Agent(object):
         return np.linalg.norm(self.pos - point)
 
 
-    def get_avoidance_vels(self, agent):
-        """ Returns the avoidance velocities"""
-        vels = []
+    def find_best_vel(self, neighbors):
+        """ Returns the best velocity given the neighbors and goal vel"""
 
-        return vels
+        vels = self.get_avoidance_vels(neighbors)
+
+        # Calcultate the best vel given goal_pos
+
+
+
+
+        return #new_vel
+
+
 
     def get_bound_ang(self, agent_b):
         """ Returns the left and right boundries given agent a traveling and agent b being and obstacle. """
         vec_ab = agent_b.pos - self.pos
-        theta = math.atan2()
+        theta = atan2(vec_ab[1], vec_ab[0])
+        print(theta)
 
-        return ang_right, ang_left
+        point_a_bvel = self.pos + agent_b.vel  # Add b's velocity to the location of a
 
 
+
+        return #ang_right, ang_left
+
+
+
+
+    def get_avoidance_vels(self, neighbors):
+        """ Returns the velocities for all neighborsthat avoids collitions"""
+
+
+        for neighbor in neighbors:
+
+            angs = [self.get_bound_ang(neighbor)]
+
+            # try different vewlocities from the solfjäder that does not colide with [angs]
+
+
+
+
+        vels = []
+
+        return vels
+
+
+
+
+# Test code
+hej = Agent([3, 1], [10, 10])
+get_bound_ang()
