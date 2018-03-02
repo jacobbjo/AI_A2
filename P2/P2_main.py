cb7deb499ed0918310299dabd8073d8cb6160641
Johanna Bromark <johanna.bromark@gmail.com>
2018-03-02 22:59:49 +0100

    Started P2

---
+++ P2/P2_main.py
@@ -0,0 +1,113 @@
+from importJSON2 import Problem
+import numpy as np
+import matplotlib.pyplot as plt
+from State import *
+
+
+def calcDistance(point1, point2):
+    return np.linalg.norm(point2-point1)
+
+def assignPoints(points, starts, goals):
+    routes = []
+    for i in range(starts.shape[0]):
+        routes.append([])
+
+    for i in range(len(points)):
+        min_distance = float("infinity")
+        min_index = -1
+        for j in range(starts.shape[0]):
+            distance = min(calcDistance(points[i].xy, starts[j]), calcDistance(points[i].xy, goals[j]))
+            if distance < min_distance:
+                min_distance = distance
+                min_index = j
+        routes[min_index].append(points[i])
+    state = State([])
+    for i in range(len(routes)):
+        route = routes[i]
+        route_object = Route(starts[i], goals[i], route)
+        state.add_route(route_object)
+
+    return state
+
+
+def create_points(point_list):
+    points = []
+    for i in range(len(point_list)):
+        new_point = Point(point_list[i], i)
+        points.append(new_point)
+    return points
+
+def main():
+
+    # Var ska ska skiftet i routerna göras och tabuerna hållas koll på?
+    # Hur ska skiftena göras?
+    # Vilka skiften ska göras?
+
+    print("hello world")
+    the_map = Problem("P22.json")
+    points = create_points(the_map.points_of_interest)
+    starts = the_map.start_positions
+    goals = the_map.goal_positions
+    # Assign each point the the agent with the closest start or goalPoint
+    init_state = assignPoints(points, starts, goals)
+    init_routes = init_state.routes
+    colors = createColorDictDist()
+    # Show the point assignments
+
+
+    for agent_index in range(len(init_routes)):
+        color = colors[agent_index+1]
+        for i in range(init_routes[agent_index].num_points):
+            plt.plot(init_routes[agent_index].route[i].xy[0], init_routes[agent_index].route[i].xy[1], "o", c=color)
+
+        plt.plot(starts[agent_index][0], starts[agent_index][1], "x", c=color)
+        plt.plot(goals[agent_index][0], goals[agent_index][1], "x", c=color)
+
+    #for agent_index in range(len(init_routes)):
+    #    color = colors[agent_index+1]
+    #    for i in range(len(init_routes[agent_index])):
+    #        plt.plot(init_routes[agent_index][i][0], init_routes[agent_index][i][1], "o", c=color)
+    #
+    #    plt.plot(starts[agent_index][0], starts[agent_index][1], "x", c=color)
+    #    plt.plot(goals[agent_index][0], goals[agent_index][1], "x", c=color)
+
+    plt.show()
+
+
+def createColorDictDist():
+    # It is 29 districts
+    colorDict = {}
+    colorDict[1] = "#0000ff" # Blue
+    colorDict[2] = "#000099" # Dark blue
+    colorDict[3] = "#0099cc" # Light blue
+    colorDict[4] = "#33cc33" # Green
+    colorDict[5] = "#006600" # Dark Green
+    colorDict[6] = "#99ff33" # Light green
+    colorDict[7] = "#9900ff" # Purple
+    colorDict[8] = "#6600cc" # Dark Purple
+    colorDict[9] = "#9966ff" # Light purple
+    colorDict[10] = "#cc00cc" # Pink
+    colorDict[11] = "#990099" # Dark pink
+    colorDict[12] = "#ff66ff" # Light pink
+    colorDict[13] = "#ff0000" # Red
+    colorDict[14] = "#800000" # Dark red
+    colorDict[15] = "#ff5050" # Light red
+    colorDict[16] = "#ffff00" # Yellow
+    colorDict[17] = "#996600" # light brown
+    colorDict[18] = "#ffff66" # Light Yellow
+    colorDict[19] = "#666666" # Grey
+    colorDict[20] = "#262626" # Dark grey
+    colorDict[21] = "#cccccc" # Light Grey
+    colorDict[22] = "#4d2600" # Brown
+    colorDict[23] = "#ff9900" # Orange
+    colorDict[24] = "#ff9933" # Light orange
+    colorDict[25] = "#00e6e6" # Turquoise
+    colorDict[26] = "#004d4d" # Dark turquoise
+    colorDict[27] = "#333300" # Strange green
+    colorDict[28] = "#cc3300" # Orange/red
+    colorDict[29] = "#000000" # Black
+    return colorDict
+
+
+if __name__ == "__main__":
+    main()
---
+++ P2/State.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+class Point(object):
+
+    def __init__(self, xy, ind):
+        self.xy = xy
+        self.index = ind
+
+class Route(object):
+
+    def __init__(self, start, goal, route_list):
+        self.start = start
+        self.goal = goal
+        self.route = route_list
+        self.tot_distance = self.calc_tot_distance()
+        self.num_points = len(self.route)
+
+    def calc_tot_distance(self):
+        tot_distance = 0
+        if len(self.route) > 0:
+            tot_distance += np.linalg.norm(self.route[0].xy - self.start)
+            tot_distance += np.linalg.norm(self.goal - self.route[len(self.route)-1].xy)
+            for i in range(len(self.route)-1):
+                tot_distance += np.linalg.norm(self.route[i+1].xy - self.route[i].xy)
+        return tot_distance
+
+    def two_opt(self):
+        for i in range(self.num_points - 2):
+            for j in range(i+2, self.num_points-1):
+                point1 = self.route[i]
+                point2 = self.route[i+1]
+                point3 = self.route[j]
+                point4 = self.route[j+1]
+                original_dist = np.linalg.norm(point1.xy - point2.xy) + np.linalg.norm(point3.xy - point4.xy)
+                changed_dist = np.linalg.norm(point1.xy - point3.xy) + np.linalg.norm(point2.xy - point4.xy)
+                if changed_dist < original_dist:
+                    self.route = self.change_route(i+1, j+1)
+
+    def change_route(self, ind_to_change1, ind_to_change2):
+        new_route = []
+        for i in range(0, ind_to_change1):
+            new_route.append(self.route[i])
+        for i in range(ind_to_change1, )
+
+
+
+class State(object):
+
+    def __init__(self, routes):
+        """ Routes are a list with route objects"""
+        self.routes = routes
+        self.routes_distance = self.calc_routes_distance(self.routes)
+
+    def calc_routes_distance(self, routes):
+        tot_distance = 0
+        for route in routes:
+            tot_distance = route.tot_distance
+        return tot_distance
+
+    def add_route(self, new_route):
+        self.routes.append(new_route)
+        self.routes_distance += new_route.tot_distance
+
