c79435794bb107799666c13f2ebf80b99bfff451
Johanna Bromark <johanna.bromark@gmail.com>
2018-03-23 13:34:19 +0100

    Started P5

---
+++ P5/P5_main.py
@@ -0,0 +1,17 @@
+from P5.importJSON5 import Problem
+from Common.agent import Agent
+from Common.functions import *
+
+# Best: 25.2
+
+def main():
+    the_map = Problem("P25.json", "P25_26_traj.json")
+
+    print(len(the_map.leader_positions))
+
+
+
+
+
+if __name__ == "__main__":
+    main()
